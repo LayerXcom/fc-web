e843104b3948c26a029542354127d56197b4be10
y-matsuwitter <info@yuki-matsumoto.com>
2018-07-25 18:19:19 +0900

    Remove unnecessary files, added some middleware and ant.design sample.
    Added below middleware.
    - redux-logger
    - redux-thunk
    - connected-react-router
    
    Added react-router implementation.
    Added ant.design sider sample.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { Switch, Route } from 'react-router-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history';
+import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router';
 import './index.css';
-import App from './containers';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const history = createBrowserHistory();
+
+const store = createStore(
+  connectRouter(history)(reducers),
+  compose(
+    applyMiddleware(
+      routerMiddleware(history), // for dispatching history actions.
+      logger, // for redux action logging.
+      thunk,  // for asynchronous actions.
+    ),
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/hoge">
+          <span>hoge</span>
+        </Route>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'));
 
